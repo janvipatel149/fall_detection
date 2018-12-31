2efd0665ee411962b49c8d016fff191b679ad11e
janvipatel49 <janvipatel49@gmail.com>
2018-12-31 10:59:19 +0530

    first commit

---
+++ fall_lib.py
@@ -0,0 +1,17 @@
+"""
+fall detection modules
+"""
+
+
+def first_module(arg):
+    ##algo
+    
+    print("module create successfully")
+    return(arg)
+
+class First:
+    def __init__(self):
+        print("class created")
+
+    def first(self):
+        print("method")
